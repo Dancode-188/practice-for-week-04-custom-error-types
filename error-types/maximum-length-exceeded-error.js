6918140a8dd65dadeb2cc0e61c673c1dde1cf1c9
Dancode-188 <danbitengo@gmail.com>
2024-09-22 11:49:05 +0300

    Practice on custom error types by extending the Error class

--- error-types/maximum-length-exceeded-error.js
+++ error-types/maximum-length-exceeded-error.js
@@ -1,6 +1,21 @@
 const ValidationError = require('./validation-error');
 
 // Your code here
+class MaximumLengthExceededError extends ValidationError {
+  constructor(excessLength) {
+
+    let message;
+    if (excessLength) {
+      message = `Maximum length exceeded by ${excessLength}`;
+    } else {
+      message = 'Maximum length exceeded';
+    }
+    super(message);
+
+
+    this.name = 'MaximumLengthExceededError';
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
--- error-types/validation-error.js
+++ error-types/validation-error.js
@@ -1,4 +1,15 @@
 // Your code here
+class ValidationError extends Error {
+  constructor(message = "Invalid input") {
+    super(message);
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, ValidationError);
+    }
+
+    this.name = 'ValidationError';
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
